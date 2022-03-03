97942aaaeaa62c8542e9407479a8e9cdc3bff1fe
SanAntonik <sanantonik@gmail.com>
2022-03-03 11:59:02 +0200

    3 figures with 16 and 4 subplots, and 4 graphs on the one plot respectively

--- random_walk.py
+++ random_walk.py
@@ -8,13 +8,13 @@ import numpy as np
 coin_flip = [1, -1]
 n = 1000
 
+
 tosses = []
 ys = []
 for i in range(0, 16):
     tosses.append([random.choice(coin_flip) for x in range(0, n)])
     ys.append([sum(tosses[i][0:x]) for x in range(0, n)])
-print(ys)
-
+# print(ys)
 
 fig, ((ax1, ax2, ax3, ax4), (ax5, ax6, ax7, ax8), (ax9, ax10, ax11, ax12), (ax13, ax14, ax15, ax16)) = plt.subplots(4, 4)
 ax1.plot(ys[0])
@@ -36,7 +36,37 @@ ax16.plot(ys[15])
 
 for ax in fig.get_axes():
     ax.label_outer()
+    ax.axhline(y=0, color="black", linestyle="-")
+plt.show()
 
+
+tosses = []
+ys = []
+for i in range(0, 4):
+    tosses.append([random.choice(coin_flip) for x in range(0, n)])
+    ys.append([sum(tosses[i][0:x]) for x in range(0, n)])
+
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+ax1.plot(ys[0])
+ax2.plot(ys[1])
+ax3.plot(ys[2])
+ax4.plot(ys[3])
+
+for ax in fig.get_axes():
+    ax.label_outer()
+    ax.axhline(y=0, color="black", linestyle="-")
+plt.show()
+
+
+tosses = []
+ys = []
+for i in range(0, 4):
+    tosses.append([random.choice(coin_flip) for x in range(0, n)])
+    ys.append([sum(tosses[i][0:x]) for x in range(0, n)])
+
+for y in ys:
+    plt.plot(y)
+plt.axhline(y=0, color="black", linestyle="-")
 plt.show()
 
     # plt.plot(tosses)
