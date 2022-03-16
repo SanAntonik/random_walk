57199de227c872824c81789105b23e5e7ab138cc
SanAntonik <sanantonik@gmail.com>
2022-03-16 19:24:20 +0200

    Add computation count and small prettifying of code

--- random_walk.py
+++ random_walk.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
-"""After running a bunch of tests, I found out that:
+
+"""
+After running a bunch of tests, I found out that:
 1. it's better to call built-in functions directly without custom functions
 2. It's better to create one variable once and use it a lot of times instead of creating the same variable over and over again
 """
@@ -11,9 +13,6 @@ import time
 
 # heads = 1 (one step forward) and tails = -1 (one step backwards)
 coin_flip = [1, -1]
-n = 2000
-
-
 def calculate(n):
     tosses = [random.choice(coin_flip) for x in range(0, n)]
     ys = [0]
@@ -25,7 +24,8 @@ def calculate(n):
 
 
 # random walk in 1D and 2D
-def walk(n, nrows=1, ncols=1, plots_per_figure=1, in_2D=False):
+def walk(n, nrows=1, ncols=1, plots_per_figure=1, in_2D=False, show_computation_time=False):
+    start = time.time()
     fig, axes = plt.subplots(nrows, ncols)
     # plt.plot(calculate(n), color="purple")
     # plt.show()
@@ -36,11 +36,16 @@ def walk(n, nrows=1, ncols=1, plots_per_figure=1, in_2D=False):
                     col.plot(calculate(n), calculate(n))
                 else:
                     col.plot(calculate(n))    
+    end = time.time()
+    if show_computation_time:
+        print(f"Elapsed time: {end - start}")
     plt.show()
 
 
-walk(n, nrows=1, ncols=1, plots_per_figure=10, in_2D=True)
-
+if __name__=='__main__':
+    n = 200000
+    walk(n, nrows=2, ncols=3, plots_per_figure=10, in_2D=False, show_computation_time=True)
+    
 
 # for i in range(1):
 #     test_figures_count = 5
